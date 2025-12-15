527aba801f8e1865832618a45a41ad7d01ddd7ce
Shelby Potts <shelbypotts.connect@gmail.com>
2025-12-14 18:12:01 -0600

    day 4 part 2

--- day_4_printing_department.py
+++ day_4_printing_department.py
@@ -1,20 +1,17 @@
 from utils.data_loader import load_input
 
-def main():
-    grid = load_input(4, 'input')
-    print(grid)
 
+def remove_rolls(grid):
     directions = [
         (-1, -1), (-1, 0), (-1, 1),
         ( 0, -1),          ( 0, 1),
         ( 1, -1), ( 1, 0), ( 1, 1)
     ]
-
     rows = len(grid)
     cols = len(grid[0])
 
     accessible_count = 0
-    
+
     # for each row and column
     for row in range(rows):
         for col in range(cols):
@@ -31,14 +28,31 @@ def main():
                             
                         if neighboring_value == '@':
                             roll_count += 1
-                            print(f"neighbor_row: {neighbor_row}, neighbor_col: {neighbor_col}, roll_count: {roll_count}")
+                            # print(f"neighbor_row: {neighbor_row}, neighbor_col: {neighbor_col}, roll_count: {roll_count}")
                     else:
                         continue
 
                 if roll_count < 4:
+                    grid[row][col] = '.'
                     accessible_count += 1 
         
-    print(f"accessibility_count: {accessible_count}")
+    return accessible_count
+
+
+def main():
+    grid = load_input(4, 'sample')
+    grid = [list(row) for row in grid]
+
+    total_removed = 0
+    while True:
+        accessible_count = remove_rolls(grid)
+
+        if accessible_count == 0:
+            break
+
+        total_removed += accessible_count
+   
+    print(f"total: {total_removed}")
 
 
 if __name__ == '__main__':
