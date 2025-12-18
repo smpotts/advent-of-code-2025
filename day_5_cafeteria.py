79b44d4f2c0c1518c649de40846bcef87bd81d97
Shelby Potts <shelby.potts@apptegy.com>
2025-12-18 11:32:21 -0600

    wip day 5 part 2

--- day_5_cafeteria.py
+++ day_5_cafeteria.py
@@ -1,5 +1,21 @@
 from utils.data_loader import load_input
 
+def get_fresh_ids(fresh_id_ranges):
+    # print(fresh_id_ranges)
+
+    range_pairs = []
+    for pair in fresh_id_ranges:
+       start, end = map(int, pair.split("-")) 
+       range_pairs.append((start, end))
+    
+    fresh_ingredients = set()
+    for pair in range_pairs:
+        for i in range(pair[0], pair[1]+1):
+            fresh_ingredients.add(i)
+
+    return fresh_ingredients
+
+
 def main():
     fresh_id_ranges, available_ids = load_input(5, 'input', comma_delimiter=False, newline_separator=True)
     # print(fresh_id_ranges)
@@ -18,7 +34,10 @@ def main():
                 # print("found a fresh one!")
                 break
 
-    print(f"fresh total: {fresh}")
+    # print(f"fresh total: {fresh}")
+
+    fresh_ingredients = get_fresh_ids(fresh_id_ranges)
+    print(f"fresh ingredients total: {len(fresh_ingredients)}")
 
 if __name__ == '__main__':
     main()
