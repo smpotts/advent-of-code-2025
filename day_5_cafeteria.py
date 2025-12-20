3daebb2c365ba5e5ab8840ee2cb19a65417fc834
Shelby Potts <shelbypotts.connect@gmail.com>
2025-12-19 22:06:37 -0600

    day 5 part 2

--- day_5_cafeteria.py
+++ day_5_cafeteria.py
@@ -1,16 +1,36 @@
 from utils.data_loader import load_input
 
 def get_fresh_ids(fresh_id_ranges):
-    # print(fresh_id_ranges)
-
-    fresh_ingredients = set()
+    range_pairs = []
     for pair in fresh_id_ranges:
         start, end = map(int, pair.split("-")) 
-    #    range_pairs.append((start, end))
-        for i in range(start, end+1):
-            fresh_ingredients.add(i)
+        range_pairs.append((start, end))
+   
+    # sort by the start
+    range_pairs.sort()
+    # print(range_pairs)
 
-    return len(fresh_ingredients)
+    # merge overlapping ranges
+    flattened_ranges = []
+    previous_start, previous_end = range_pairs[0]
+
+    for start, end in range_pairs:
+        # print(f"range_pair: {start}, {end}")
+        if previous_start <= start <= previous_end:
+            # print(f"overlap: {previous_start} <= {start} <= {end}")
+            previous_end = max(previous_end, end)
+        else:
+            flattened_ranges.append((previous_start, previous_end))
+            previous_start = start
+            previous_end = end 
+
+    flattened_ranges.append((previous_start, previous_end))
+    
+    total = 0
+    for start, end in flattened_ranges:
+        total += (end - start + 1)
+
+    return total 
 
 
 def main():
